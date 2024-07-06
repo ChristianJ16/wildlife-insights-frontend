d98c55e3dbfcd1e0d74e7b6fc3e8fa492f9eba76
christianj16 <christianjansen16@gmail.com>
2024-07-06 01:07:45 -0700

    updated for mobile

--- src/components/AnimalInfo.js
+++ src/components/AnimalInfo.js
@@ -13,22 +13,19 @@ const AnimalInfo = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false)
     const [error, setError] = useState(null)
 
-    
-    
     const animals = [
         "Aardvark", "Alligator", "Alpaca", "Anaconda", "Ant", "Anteater", "Antelope", "Aphid", "Armadillo", "Asp", "Ass",
         "Baboon", "Badger", "Bald Eagle", "Barracuda", "Bass", "Basset Hound", "Bat", "Bearded Dragon", "Beaver", "Bedbug",
         "Bee", "Bee-eater", "Bird", "Bison", "Black panther", "Black Widow Spider", "Blue Jay", "Blue Whale", "Bobcat", 
-        "Buffalo", "Butterfly", "Buzzard", "Camel", "Canada Lynx", "Carp", "Cat", "Caterpillar", "Catfish", "Cheetah", 
-        "Chicken", "Chimpanzee", "Chipmunk", "Cobra", "Cod", "Condor", "Cougar", "Cow", "Coyote", "Crab", "Crane Fly", 
-        "Cricket", "Crocodile", "Crow", "Cuckoo", "Deer", "Dinosaur", "Dog", "Dolphin", "Donkey", "Dove", "Dragonfly", 
-        "Duck", "Eagle", "Eel", "Elephant", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Flea", "Fly", "Fox", 
+        "Buffalo", "Buzzard", "Camel", "Canada Lynx", "Carp", "Cat", "Catfish", "Cheetah", 
+        "Chicken", "Chimpanzee", "Chipmunk", "Cobra", "Cod", "Condor", "Cougar", "Cow", "Coyote", "Crab", "Crocodile", "Crow", "Cuckoo", "Deer", "Dinosaur", "Dog", "Dolphin", "Donkey", "Dove", 
+        "Duck", "Eagle", "Eel", "Elephant", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fox", 
         "Frog", "Goat", "Goose", "Gopher", "Gorilla", "Guinea Pig", "Hamster", "Hare", "Hawk", "Hippopotamus", "Horse", 
         "Hummingbird", "Humpback Whale", "Husky", "Iguana", "Impala", "Kangaroo", "Lemur", "Leopard", "Lion", "Lizard", 
         "Llama", "Lobster", "Margay", "Monitor lizard", "Monkey", "Moose", "Mosquito", "Moth", "Mountain Zebra", "Mouse", 
         "Mule", "Octopus", "Orca", "Ostrich", "Otter", "Owl", "Ox", "Oyster", "Panda", "Parrot", "Peacock", "Pelican", 
         "Penguin", "Perch", "Pheasant", "Pig", "Pigeon", "Polar bear", "Porcupine", "Quagga", "Rabbit", "Raccoon", "Rat", 
-        "Rattlesnake", "Red Wolf", "Rooster", "Seal", "Sheep", "Skunk", "Sloth", "Snail", "Snake", "Spider", "Tiger", 
+        "Rattlesnake", "Red Wolf", "Rooster", "Seal", "Sheep", "Skunk", "Sloth", "Snake", "Spider", "Tiger", 
         "Whale", "Wolf", "Wombat", "Zebra"
     ]
 
@@ -69,36 +66,32 @@ const AnimalInfo = () => {
         <div>
             <Header />
             <div className="container mx-auto p-4">
-                <div className="flex items-center justify-center mb-4 mt-52" >
+                <div className="flex flex-col sm:flex-row items-center justify-center mb-4 mt-36 space-y-2 sm:space-y-0 sm:space-x-2 w-full max-w-lg mx-auto">
                     <input
                         type="text"
                         value={animalName}
                         onChange={(e) => setAnimalName(e.target.value)}
                         placeholder="Enter animal name..."
-                        className="border rounded p-2 w-96 outline-none"style={{
-                            boxShadow: '0 0 10px black'
-                        }}
+                        className="border rounded p-2 w-full outline-none"
+                        style={{ boxShadow: '0 0 10px black' }}
                     />
                     <button
                         onClick={handleSearchClick}
-                        className="ml-2 p-2 bg-blue-500 text-white rounded hover:bg-blue-700"style={{
-                            boxShadow: '0 0 10px black'
-                        }}
+                        className="p-2 bg-blue-500 text-white rounded hover:bg-blue-700 w-full sm:w-auto"
+                        style={{ boxShadow: '0 0 10px black' }}
                     >
                         Search
                     </button>
-                    
                     <button
                         onClick={handleRandomClick}
-                        className="ml-2 p-2 bg-purple-500 text-white rounded hover:bg-purple-700"style={{
-                            boxShadow: '0 0 10px black'
-                        }}
+                        className="p-2 bg-purple-500 text-white rounded hover:bg-purple-700 w-full sm:w-auto"
+                        style={{ boxShadow: '0 0 10px black' }}
                     >
                         Random!
                     </button>
-                    </div>
-                {error && <p className="text-red-500 mb-4">{error}</p>}
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8 justify-center">
+                </div>
+                {error && <p className="text-red-500 mb-4 text-center">{error}</p>}
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {animalData.map((animal, index) => (
                         <div key={index} className="flex justify-center">
                             <AnimalCard animal={animal} onClick={openModal} />
